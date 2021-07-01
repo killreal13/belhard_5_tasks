59e3e7c1a051cd035d510798ace807e0fe84e1dd
yungun <kirill.dyuzhij@gmail.com>
2021-07-01 16:13:08 +0300

    fix solves
    *most_common_and_longest

--- tasks/hard/most_common_and_longest.py
+++ tasks/hard/most_common_and_longest.py
@@ -7,12 +7,17 @@
 можно решить с помощью циклов и переменных, но предпочтительней с
 помощью модуля collections, используя collections.Counter
 """
+import collections
 
 
 def common_and_longest(text: str) -> tuple:
-    common = None
-    longest = None
-    return common, longest
+    common = collections.Counter(text.split(' ')).most_common(1)
+    splited_text = text.split(' ')
+    for i in range(len(text.split(' '))):
+        if len(splited_text[0]) < len(splited_text[i]):
+            splited_text[0], splited_text[i] = splited_text[i], splited_text[0]
+            longest = splited_text[0]
+    return common[0][0], longest
 
 
 if __name__ == '__main__':
